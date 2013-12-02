ef434661b3fac41700899420afea266543db5333
Scott Bader <scott@renttherunway.com>
2013-12-02 15:33:32 -0500

    Not creating color files

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,17 +20,6 @@ class dotfiles {
 
   snippet { ['all', 'coffee', 'rails', 'ruby', 'sh', 'snippets', 'vim', 'xdebug']: }
 
-  file { "${home}/.vim/colors":
-    ensure => 'directory',
-    recurse => true
-  }
-
-  file { "${dotfiles::home}/.vim/colors/candy-sbader.vim":
-    ensure  => 'present',
-    source  => "puppet:///modules/dotfiles/candy-sbader.vim",
-    require => File["${dotfiles::home}/.vim/colors"]
-  }
-
   file { "${home}/.zsh":
     ensure => 'directory'
   }
